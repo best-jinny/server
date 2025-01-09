48324aa613a3548a7495b47a7fb145fd3e6e29c4
hyejin <iwilldotherightthings@gmail.com>
2025-01-10 01:08:09 +0900

    주문 서비스

---
+++ src/main/java/kr/hhplus/be/server/order/infra/OrderRepositoryImpl.java
@@ -0,0 +1,18 @@
+package kr.hhplus.be.server.order.infra;
+
+import kr.hhplus.be.server.order.domain.Order;
+import kr.hhplus.be.server.order.domain.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryImpl implements OrderRepository {
+
+    private final OrderJpaRepository orderJpaRepository;
+
+    @Override
+    public Order save(Order order) {
+        return orderJpaRepository.save(order);
+    }
+}
