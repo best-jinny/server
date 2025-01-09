09a03e99e336161d3d7dd6b56fa8cffc0612a00b
hyejin <iwilldotherightthings@gmail.com>
2025-01-10 01:13:29 +0900

    주문 api 완료

--- src/main/java/kr/hhplus/be/server/order/interfaces/controller/OrderRequest.java
+++ src/main/java/kr/hhplus/be/server/order/interfaces/controller/OrderRequest.java
@@ -1,14 +1,45 @@
 package kr.hhplus.be.server.order.interfaces.controller;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderRequest {
-    private String customerId;
-    private List<OrderItemRequest> orderItems;
-    private String couponCode;
+
+    @NotNull
+    private Long userId;
+
+    @NotEmpty
+    private List<OrderLineRequest> orderLines;
+
+    private Long couponId;
+
+    public OrderRequest(Long userId, List<OrderLineRequest> orderLines) {
+        this.userId = userId;
+        this.orderLines = orderLines;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class OrderLineRequest {
+
+        @NotNull
+        private Long productId;
+
+        @NotNull
+        private int quantity;
+
+        @NotNull
+        private Long price;
+    }
+
 }
