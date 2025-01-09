1e70265ecebaef10ed3bdbc334b3b2d10a36a51c
hyejin <iwilldotherightthings@gmail.com>
2025-01-10 01:13:29 +0900

    주문 api 완료

---
+++ src/main/java/kr/hhplus/be/server/order/infra/DataPlatform.java
@@ -0,0 +1,11 @@
+package kr.hhplus.be.server.order.infra;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataPlatform {
+
+    public boolean send() {
+        return true;
+    }
+}
