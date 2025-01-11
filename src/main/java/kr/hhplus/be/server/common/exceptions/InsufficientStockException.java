13343c527f0bdef8c623faddce47a0f1b6852e22
hyejin <iwilldotherightthings@gmail.com>
2025-01-11 21:22:16 +0900

    포인트 단위 테스트

--- src/main/java/kr/hhplus/be/server/common/exceptions/InsufficientStockException.java
+++
@@ -1,7 +0,0 @@
-package kr.hhplus.be.server.common.exceptions;
-
-public class InsufficientStockException extends RuntimeException {
-    public InsufficientStockException(String message) {
-        super(message);
-    }
-}
