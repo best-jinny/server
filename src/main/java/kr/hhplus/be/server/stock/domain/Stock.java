ce656bdb5bc8c060596ab155fcf766c32c7368a7
hyejin <iwilldotherightthings@gmail.com>
2025-01-11 22:12:13 +0900

    재고 단위 테스트

--- src/main/java/kr/hhplus/be/server/stock/domain/Stock.java
+++ src/main/java/kr/hhplus/be/server/stock/domain/Stock.java
@@ -2,8 +2,10 @@ package kr.hhplus.be.server.stock.domain;
 
 import jakarta.persistence.*;
 import kr.hhplus.be.server.common.entity.BaseTimeEntity;
+import kr.hhplus.be.server.common.exceptions.InvalidException;
 import kr.hhplus.be.server.common.exceptions.NotEnoughException;
 import kr.hhplus.be.server.product.domain.Product;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,12 +18,20 @@ public class Stock extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    private Product product;
+    private Long productId;
 
     private int quantity;
 
+    @Builder
+    public Stock(Long productId, int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+
     public void deduct(int amount) {
+        if(amount <= 0) {
+            throw new IllegalArgumentException("유효하지 않은 수량");
+        }
         if(this.quantity - amount < 0) {
             throw new NotEnoughException("재고가 부족합니다");
         }
