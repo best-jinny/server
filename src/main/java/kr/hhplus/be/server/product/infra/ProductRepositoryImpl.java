1b22128445105c44e3b58846076a193403f5e7ce
hyejin <iwilldotherightthings@gmail.com>
2025-01-12 19:16:33 +0900

    상품 조회 api

--- src/main/java/kr/hhplus/be/server/product/infra/ProductRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/product/infra/ProductRepositoryImpl.java
@@ -3,6 +3,8 @@ package kr.hhplus.be.server.product.infra;
 import kr.hhplus.be.server.product.domain.Product;
 import kr.hhplus.be.server.product.domain.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,8 +17,8 @@ public class ProductRepositoryImpl implements ProductRepository {
     private final ProductJpaRepository productJpaRepository;
 
     @Override
-    public List<Product> findAll() {
-        return List.of();
+    public Page<Product> findAll(Pageable pageable) {
+        return productJpaRepository.findAll(pageable);
     }
 
     @Override
