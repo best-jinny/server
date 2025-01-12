e3eea91d2e175aaf2a838666a2520987834a4eee
hyejin <iwilldotherightthings@gmail.com>
2025-01-12 23:39:41 +0900

    판매 상위 상품 조회 api

--- src/main/java/kr/hhplus/be/server/order/domain/OrderService.java
+++ src/main/java/kr/hhplus/be/server/order/domain/OrderService.java
@@ -3,6 +3,9 @@ package kr.hhplus.be.server.order.domain;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class OrderService {
@@ -12,5 +15,9 @@ public class OrderService {
     public Order completeOrder(Order order) {
         order.complete();
         return orderRepository.save(order);
+    }
+
+    public List<TopProductsResult> getTopProducts() {
+        return orderRepository.findTopProducts(LocalDateTime.now().minusDays(2), 5);
     }
 }
--- src/main/java/kr/hhplus/be/server/product/interfaces/controller/ProductController.java
+++ src/main/java/kr/hhplus/be/server/product/interfaces/controller/ProductController.java
@@ -1,22 +1,23 @@
 package kr.hhplus.be.server.product.interfaces.controller;
 
 import kr.hhplus.be.server.common.dto.PageResponse;
-import kr.hhplus.be.server.coupon.domain.IssuedCouponResult;
+import kr.hhplus.be.server.order.domain.OrderService;
 import kr.hhplus.be.server.product.domain.ProductResult;
 import kr.hhplus.be.server.product.domain.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
     private final ProductService productService;
+    private final OrderService orderService;
 
     // 전체 상품 조회
     @GetMapping()
@@ -26,19 +27,5 @@ public class ProductController {
         Page<ProductResult> products = productService.getProducts(page, size);
         PageResponse<ProductResult> pageResponse = PageResponse.of(products);
         return ResponseEntity.ok(pageResponse);
-    }
-
-    // 최근 3일간 판매량 top 5 상품 조회
-    @GetMapping("/top-sellers")
-    public ResponseEntity<?> getTopSellers() {
-        List<TopSellerResponse> topSellers = new ArrayList<>();
-
-        topSellers.add(new TopSellerResponse("p1", "상품A", 500));
-        topSellers.add(new TopSellerResponse("p34", "상품B", 350));
-        topSellers.add(new TopSellerResponse("p23", "상품C", 210));
-        topSellers.add(new TopSellerResponse("p15", "상품D", 180));
-        topSellers.add(new TopSellerResponse("p9", "상품E", 150));
-
-        return ResponseEntity.ok(topSellers);
     }
 }
